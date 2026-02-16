ab29fc1c5a8066c9652960ac5ade621ff653e548
peternicolas1010 <rouyar.angela@gmail.com>
2026-02-16 00:41:33 +0000

    make up pratice lesson 1

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,36 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
 
-
+favorite_number=(7)
+print(favorite_number)
 
 # Problem 2
 # Create a variable for your height in meters and print it.
 
-
-
+peter_height=1.39
+print(peter_height)
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
 
-
+favorite_animal="octopus"
+print("My favorite animal is",favorite_animal)
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
 
+first_name="Peter"
+print(first_name)
 
+last_name="Nicolas"
+print(last_name)
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+
+peter_age=10
+
+favorite_color="yellow"
+
+print("My name is",first_name,"my age is",peter_age,"and my favorite color is",favorite_color)
