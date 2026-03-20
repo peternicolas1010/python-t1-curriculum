a88f43a94ab7cdf8227bb071de642a3a6ecdac6a
peternicolas1010 <rouyar.angela@gmail.com>
2026-03-20 01:42:57 +0000

    finish homework5

--- lesson3/classwork/random_number.py
+++ lesson3/classwork/random_number.py
@@ -1,7 +1,9 @@
-import random # You must import the random module to use randome number finction
+import random # You must import the random module to use randome number function
 
 num = random.randint(1, 10)  # Picks a randome number between 1 and 10 ()
 print(num)
 for i in range(10):
     num2 = random.randint(1, 100)
     print(num2)
+
+
--- lesson5/homework/problems.py
+++ lesson5/homework/problems.py
@@ -12,23 +12,37 @@ import random
 # Print the second subject.
 # Then sort them alphabetically and print the result.
 
+subjects = ["math", "write", "read", "lunch",]
 
+print(subjects[2])
+
+# Sort them alphabetically
+subjects.sort()
+print(subjects)
 
 # Problem 3 
 # Create a list of 5 error codes.
 # Print how many there are.
 # Then find the index of "403" and print it.
+errors = ["300", "230", "403", "843", "500",]
 
-
+print(len(errors))
+print(errors.index("403"))
 
 # Problem 4 
 # Create a list of 2 programming languages.
 # Print a random one.
 # Then append another language and print the list.
+import random
+
+languages = ["french","english",]
+languages2 = ["french","english", "spanish",]
+language = random.choice(languages)  # Picks a randome language
+print(language)
+print(languages2)
 
 
 
-# Problem 5
-# Create a list of 6 passwords.
+# or Create a list of 6 passwords.
 # Print the one in the middle using len().
 # Then remove the first password in the list and print it.
